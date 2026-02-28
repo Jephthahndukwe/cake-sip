fb8bcf0de988c67bbe6b9adc11e087af6f1ad8fd
Jephthah Ndukwe <jephthahndukwe@gmail.com>
2026-02-28 12:35:03 +0100

    new update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Poppins, Montserrat } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
@@ -22,35 +23,58 @@ const montserrat = Montserrat({
 
 export const metadata: Metadata = {
   title: "Cake & Sip 2.0 - Learn. Decorate. Vibe. Eat.",
-  description: "Join us for an unforgettable cake decorating experience with cocktails, karaoke, games, and amazing vibes.",
+  description:
+    "Join us for an unforgettable cake decorating experience with cocktails, karaoke, games, and amazing vibes.",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <head>
-        {/* TikTok Pixel Code Start */}
-        <script
+        {/* TikTok Pixel */}
+        <Script
+          id="tiktok-pixel"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-!function (w, d, t) {
-  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
-var e=ttq._i[t][],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t{},ttq._t[e]=+new Date,ttq._o=ttq._o{},ttq._o[e]=n||{};n=document.createElement("script")
-;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+              !function (w, d, t) {
+                w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];
+                ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
+                ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}; 
+                for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
+                ttq.instance=function(t){for(
+                  var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++
+                )ttq.setAndDefer(e,ttq.methods[n]);return e};
+                ttq.load=function(e,n){
+                  var r="https://analytics.tiktok.com/i18n/pixel/events.js",
+                  o=n&&n.partner;
+                  ttq._i=ttq._i||{};
+                  ttq._i[e]=[];
+                  ttq._i[e]._u=r;
+                  ttq._t=ttq._t||{};
+                  ttq._t[e]=+new Date;
+                  ttq._o=ttq._o||{};
+                  ttq._o[e]=n||{};
+                  n=d.createElement("script");
+                  n.type="text/javascript";
+                  n.async=!0;
+                  n.src=r+"?sdkid="+e+"&lib="+t;
+                  e=d.getElementsByTagName("script")[0];
+                  e.parentNode.insertBefore(n,e)
+                };
 
-
-  ttq.load('D6HC2JRC77UCTB9KFPMG');
-  ttq.page();
-}(window, document, 'ttq');
+                ttq.load('D6HC2JRC77UCTB9KFPMG');
+                ttq.page();
+              }(window, document, 'ttq');
             `,
           }}
         />
-        {/* TikTok Pixel Code End */}
       </head>
+
       <body
         className={`${inter.variable} ${poppins.variable} ${montserrat.variable} antialiased`}
       >
